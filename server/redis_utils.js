7efa50787b920b3b7886050ab7741c934e526984
NanoClem <clemdecoop.pro@gmail.com>
2020-04-09 00:04:18 +0200

    doc

--- server/redis_utils.js
+++ server/redis_utils.js
@@ -4,7 +4,7 @@ const client = redis.createClient();
 
 /**
  * Get all elements in a list
- * @param {string} key key value of the list stored in redis
+ * @param {string} key key value of the list in redis
  */
 var getList = function (key, fn) {
     client.lrange(key, 0, -1, function (err, res) {
@@ -12,7 +12,11 @@ var getList = function (key, fn) {
     });
 }
 
-
+/**
+ * Add element to list in redis
+ * @param {string} key key value of the list in redis
+ * @param {string} elem element to add
+ */
 var addElementTo = function(key, elem) {
     client.rpush([key, JSON.stringify(elem)], function (err, res) {
         // do something on insert
@@ -20,8 +24,8 @@ var addElementTo = function(key, elem) {
 }
 
 /**
- * Remove element from a redis list
- * @param {string} key key value of the list stored in redis
+ * Remove element from a list in redis
+ * @param {string} key key value of the list in redis
  * @param {string} elem element to remove
  */
 var removeElementFrom = function (key, elem) {
