4efc40727dce639693560772f93b3b22ea4ad2ec
NanoClem <clemdecoop.pro@gmail.com>
2020-04-20 17:44:04 +0200

    implementing exists func and post routing

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -2,6 +2,27 @@ const Models = require('../models');
 
 
 /**
+ * Check if a user exists in database. If so, pass it to callback function.
+ * @param {String} name 
+ * @param {Function} fn 
+ */
+function exists(name, fn) {
+    Models.User.findOne( {username: name})
+    .exec(function (err, user) {
+        if (user != null) {
+            fn(err, user);
+        } else {
+            fn('user not found', null);
+        }
+    });
+}
+
+
+/*============================================================
+    MESSAGE CONTROLLERS
+==============================================================*/
+
+/**
  * Get all stored messages
  * @param {*} req 
  * @param {*} res 
@@ -9,6 +30,7 @@ const Models = require('../models');
 function getAllMessages(req, res) {
 
     Models.Message.find({})
+    .populate('sender')
     .exec( function (err, messages) {
         if (err) return res.json(err);
         res.json(messages);
@@ -24,22 +46,18 @@ function getAllMessages(req, res) {
 function postMessage(req, res) {
     
     // find sender
-    Models.User.findOne( {username: req.body.sender} )
-    .exec( function (err, user) {
-        if (err) throw err;
+    exists(req.body.sender, function (err, user) {
         // user not found
-        if (user == null) {
-            return res.status(404).json( {error: 'user not found'} );
-        }
+        if (err) return res.status(404).json( {error: err} );
         // create new message
         const new_message = Models.Message({
             text : req.body.text,
             sender : user._id       // save ref
         });
         // save it
-        new_message.save( function (err, message) {
-            if (err) return handleError(err);
-            res.json({
+        new_message.save( function (err2, message) {
+            if (err2) throw err2; 
+            res.status(201).json({
                 message: 'success',
                 inserted_id: message._id
             });
@@ -56,16 +74,41 @@ function postMessage(req, res) {
 function getUserMessages(req, res) {
 
     // find user
-    Models.User.findOne( {username: req.body.username} )
-    .exec( function (err, user) {
-        if (err) return handleError(err);
-
+    exists(req.body.username, function (err, user) {
+        // user not found
+        if (err) return res.status(404).json( {error: err} );
         // find all of his messages
         Models.Message.find( {sender: user._id} )
         .populate('sender')
-        .exec( function (err, messages) {
-            if (err) return handleError(err);
+        .exec( function (err2, messages) {
+            if (err2) throw err2;
             res.json(messages);
+        });
+    });
+}
+
+
+/*============================================================
+    USER CONTROLLERS
+==============================================================*/
+
+function createUser(req, res) {
+
+    // check if user already exists
+    exists(req.body.username, function (err, user) {
+        if (user != null) return res.json( {message: 'user already exists'} );
+        // create new user
+        const new_user = Models.User({      
+            username: req.body.username
+        });
+        // save it
+        new_user.save( function (err2, saved_user) {    
+            if (err2) throw err2;
+            console.log(saved_user);
+            res.status(201).json({
+                message: 'success',
+                inserted_id: saved_user._id
+            });
         });
     });
 }
@@ -76,4 +119,5 @@ module.exports = {
     getAllMessages : getAllMessages,
     getUserMessages : getUserMessages,
     postMessage : postMessage,
+    createUser : createUser
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,49 +1,77 @@
 const router = require('express').Router();
 const controller = require('../controllers');
-const base_url = '/api/message';
 
+// url
+const base_url = '/api';
+const message_url = base_url + '/message'
+const user_url = base_url + '/user'
+
+
+/*============================================================
+    MESSAGE ROUTING
+==============================================================*/
+
+/**
+ * Post a message
+ */
+router.post(message_url, (req, res) => {
+    controller.postMessage(req, res);
+});
 
 /**
  * Get all messages
  */
-router.get(base_url, (req, res) => {
+router.get(message_url, (req, res) => {
     controller.getAllMessages(req, res);
 });
 
 /**
  * Get all messages from a user
  */
-router.get(base_url + '/:username', (req, res) => {
+router.get(message_url + '/:username', (req, res) => {
     controller.getUserMessages(req, res);
 });
 
 /**
  * Get all messages up to n-minutes old
  */
-router.get(base_url + '/history', (req, res) => {
+router.get(message_url + '/history', (req, res) => {
+    // controller
+});
+
+
+/*============================================================
+    USER ROUTING
+==============================================================*/
+
+/**
+ * Create a new user
+ */
+router.post(user_url, (req, res) => {
+    controller.createUser(req, res);
+});
+
+/**
+ * Get a user
+ */
+router.get(user_url, (req, res) => {
     // controller
 });
 
 /**
  * Get the user who sent the most messages
  */
-router.get(base_url + '/bestsender', (req, res) => {
+router.get(user_url + '/bestsender', (req, res) => {
     // controller
 });
 
 /**
  * Get the user who received the most messages
  */
-router.get(base_url + '/bestreceiver', (req, res) => {
+router.get(user_url + '/bestreceiver', (req, res) => {
     // controller
 });
 
-/**
- * Post a message
- */
-router.post(base_url, (req, res) => {
-    controller.postMessage(req, res);
-});
 
 
 // Exports
